dd3768ace25215026e09027302ff7b8866cc5be1
juanpaAZum <juan.arroyo.zumbado@est.una.ac.cr>
2026-09-05 00:25:48 -0600

    User.java_created

---
+++ src/Model/User.java
@@ -0,0 +1,24 @@
+package Model;
+
+public class User {
+    //Attributes
+    private int id;
+    private String password;
+    //End of attributes
+
+    //Default builder
+    public User(){}
+    //Builder with parameters
+    public User(int id, String password){
+        this.id = id;
+        this.password = password;
+    }
+
+    //Getter methods
+    public int getId(){return id;}
+    public String getPassword(){return password;}
+
+    //Setter methods
+    public void setId(int id){this.id = id;}
+    public void setPassword(String password){this.password = password;}
+}
