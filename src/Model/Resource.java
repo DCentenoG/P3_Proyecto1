921e9c62650ab2542e193bb4c212587a6652cc22
juanpaAZum <juan.arroyo.zumbado@est.una.ac.cr>
2026-09-07 12:51:50 -0600

    fix(Juanpa AZ): Corregir errores de nomenclatura camelCase en la clase Resource y agregar comentario de excepcion en addResource de ResourceCategory.java.

--- src/Model/Resource.java
+++ src/Model/Resource.java
@@ -3,34 +3,34 @@ package Model;
 public class Resource {
     //Attributes
     private int id;
-    private ResourceCategory ResourceCategoryReference;
+    private ResourceCategory resourceCategoryReference;
     private String description;
     //End of attributes
 
     //Default Builder
     public Resource(){
         id = 0;
-        ResourceCategoryReference = null;
+        resourceCategoryReference = null;
         description = null;
     }
 
     //Parameterized builder
     public Resource(int id, ResourceCategory ResourceCategoryReference, String description) {
         this.id = id;
-        this.ResourceCategoryReference = ResourceCategoryReference;
+        this.resourceCategoryReference = ResourceCategoryReference;
         this.description = description;
     }
 
     //getters
     public int getId() {return id;}
     public String getDescription() {return description;}
-    public ResourceCategory getResourceContainerReference() {return ResourceCategoryReference;}
+    public ResourceCategory getResourceCategoryReference() {return resourceCategoryReference;}
 
     //setters
     public void setId(int id) {this.id = id;}
     public void setDescription(String description) {this.description = description;}
     public void setResourceCategoryReference(ResourceCategory ResourceCategoryReference){
-        this.ResourceCategoryReference = ResourceCategoryReference;
+        this.resourceCategoryReference = ResourceCategoryReference;
     }
 
 
