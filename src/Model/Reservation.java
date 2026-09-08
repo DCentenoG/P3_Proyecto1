8b8fbc606fc3c0b4c4798e35616234e8c7fd8113
juanpaAZum <juan.arroyo.zumbado@est.una.ac.cr>
2026-09-08 14:08:22 -0600

    test(Juanpa AZ): Agregar primera version de los metodos Overlaps y addResource en Reservation.java para verificar y agregar recursos a una reserva sin choques de horario. Tambien Corregir errores de nomenclatura CamelCase a a algunas variables (cambio no importante).

--- src/Model/Reservation.java
+++ src/Model/Reservation.java
@@ -6,27 +6,27 @@ public class Reservation {
     //Attributes
     private String Activity;
     private LocalDate date;
-    private LocalTime StartTime;
-    private LocalTime EndTime;
-    private ArrayList<Resource> AssignedResources;
+    private LocalTime startTime;
+    private LocalTime endTime;
+    private ArrayList<Resource> assignedResources;
     //End of attributes
 
     //Default Builder
     public Reservation() {
         Activity = null;
         date = null;
-        StartTime = null;
-        EndTime = null;
-        AssignedResources = new ArrayList<>();
+        startTime = null;
+        endTime = null;
+        assignedResources = new ArrayList<>();
     }
 
     //Parameterized Builder
     public Reservation(String Activity, LocalDate date, LocalTime StartTime, LocalTime EndTime) {
         this.Activity = Activity;
         this.date = date;
-        this.StartTime = StartTime;
-        this.EndTime = EndTime;
-        AssignedResources = new ArrayList<>(); //PENDIENTE VERIFICAR SI RECIBE LOS RECURSOS O SE AGREGAN CON LA RESERVACION YA CREADA
+        this.startTime = StartTime;
+        this.endTime = EndTime;
+        assignedResources = new ArrayList<>(); //PENDIENTE VERIFICAR SI RECIBE LOS RECURSOS O SE AGREGAN CON LA RESERVACION YA CREADA
     }
 
     //basic getters
@@ -39,15 +39,15 @@ public class Reservation {
     }
 
     public LocalTime getStartTime() {
-        return StartTime;
+        return startTime;
     }
 
     public LocalTime getEndTime() {
-        return EndTime;
+        return endTime;
     }
 
     public ArrayList<Resource> getAssignedResources() {
-        return AssignedResources;
+        return assignedResources;
     }
 
     //basic setters
@@ -60,18 +60,50 @@ public class Reservation {
     }
 
     public void setStartTime(LocalTime StartTime) {
-        this.StartTime = StartTime;
+        this.startTime = StartTime;
     }
 
     public void setEndTime(LocalTime EndTime) {
-        this.EndTime = EndTime;
+        this.endTime = EndTime;
     }
 
     public void setAssignedResources(ArrayList<Resource> AssignedResources) {
-        this.AssignedResources = AssignedResources;
+        this.assignedResources = AssignedResources;
     }
 
     //logic and calculus methods
 
+    //Este metodo chequea si dos reservas chocan en horario al menos un momento.
+    public boolean Overlaps(Reservation other) {
+        return this.startTime.isBefore(other.endTime)
+                && other.startTime.isBefore(this.endTime);
+    }
 
+    //Este metodo intenta agregar un recurso a la reserva verificando primero si no esta apartado en otra reserva que coincida en hora
+    /*Se decidio incluir en la reserva para evitar acoplamiento de clases innecesario, esto porque Employee igual se le implementara un
+    metodo que es llama a addResource al crear la reserva n veces segun la cantidad de categorias de recursos seleccionadas en la capa
+    de la vista*/
+    public boolean addResource(Resource resource, UserContainer users) {
+
+        //Certificates first if the resource is already on another reservation at the same time
+        ArrayList<Employee> listOfEmployeesForCheckingReservations = users.getListOfEmployees();
+        for (int i = 0; i < listOfEmployeesForCheckingReservations.size(); i++) {
+            Employee aux = listOfEmployeesForCheckingReservations.get(i);
+            ArrayList<Reservation> toCheckList = aux.getReservations();
+            for (int j = 0; j < toCheckList.size(); j++) {
+                if (this.Overlaps(toCheckList.get(j))) {
+                    for (int k = 0; k < toCheckList.get(j).getAssignedResources().size(); k++) {
+                        if (toCheckList.get(j).getAssignedResources().get(k).equals(resource)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+
+        /*if all the loop get to the last iteration it means that the resource is available at that moment
+        so you can add it and then returns true to clarify that it has been added*/
+        assignedResources.add(resource);
+        return true;
+    }
 }
